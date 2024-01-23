749cd36530d8ce325178d50ce00e1ba19f9c61e9
ahmedamin20 <ahmedamin01147@hotmail.com>
2024-01-24 00:00:05 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from './_compnent/NavBar/NavBar';
+import NavBar from "./_compnent/NavBar/NavBar";
 import Footer from "./_compnent/Footer/Footer";
-import Header from './_compnent/Header/Header';
+import Header from "./_compnent/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />r
-        <div className='m-5'></div>
+        <Header />
+        <div className="m-5"></div>
 
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
