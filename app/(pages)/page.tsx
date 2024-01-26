5b22d677df52ed92531375c4034b181885f02649
ahmedamin20 <ahmedamin01147@hotmail.com>
2024-01-26 10:30:24 +0200

    update

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,11 @@
+import Rests from "@/app/_compnent/rests/page";
+import Ads from "./../../_compnent/Ads/Ads";
+
+export default function Home() {
+  return (
+    <main>
+      <Ads />
+      <Rests />
+    </main>
+  );
+}
