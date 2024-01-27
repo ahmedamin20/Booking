955c82dcf7620f9d783889e444d933df671af525
ahmedamin20 <ahmedamin01147@hotmail.com>
2024-01-27 12:08:24 +0200

    finish

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,6 +1,12 @@
 export default function About() {
   return (
     <section class="flex items-center flex-col bg-stone-100  font-poppins dark:bg-gray-800 ">
+      <h2 class="m-8 text-center text-4xl font-semibold text-gray-800 ">
+        جنوب السعودية هو وجهة سياحية رائعة تتميز بتنوع طبيعي وثقافي فريد. تضم
+        هذه المنطقة العديد من المعالم السياحية الشهيرة والمناظر الطبيعية الخلابة
+        التي تجذب السياح من جميع أنحاء العالم. إليك نبذة عن السياحة في جنوب
+        السعودية
+      </h2>
       <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
         <div class="flex flex-wrap ">
           <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
@@ -25,6 +31,7 @@ export default function About() {
           </div>
         </div>
       </div>
+
       <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
         <div class="flex flex-wrap ">
           <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
@@ -47,6 +54,57 @@ export default function About() {
           </div>
         </div>
       </div>
+
+      <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+        <div class="flex flex-wrap ">
+          <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
+            <h2 class="mb-4 text-4xl font-semibold text-blue-500 ">
+              منطقة جازان{" "}
+            </h2>
+            <p class="mb-10 text-bold leading-7 text-gray-500 ">
+              تقع على ساحل البحر الأحمر وتتميز بشواطئها الجميلة والجزر
+              الاستوائية. يمكنك زيارة جزيرة فرسان والاستمتاع بالغوص والغطس في
+              المياه الزرقاء الصافية. كما يمكنك زيارة مدينة جازان والتجول في
+              الأسواق واستكشاف المناطق الساحلية الجميلة.
+            </p>
+          </div>
+          <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+            <img
+              src="/image3.jpg"
+              alt=""
+              class="relative z-40 object-cover w-full h-96 rounded-3xl"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+        <div class="flex flex-wrap ">
+          <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+            <img
+              src="/image4.jpg"
+              alt=""
+              class="relative z-40 object-cover w-full h-96 rounded-3xl"
+            />
+          </div>
+          <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
+            <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">
+              منطقة عرعر
+            </h2>
+            <p class="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400">
+              تقع في شمال غرب المملكة وتعتبر بوابة إلى الصحراء العربية. يمكنك
+              زيارة قلعة عرعر التاريخية ومتحف عرعر الإقليمي للاطلاع على التراث
+              الثقافي للمنطقة. كما يمكنك الاستمتاع بالتجول في الصحراء وقيادة
+              الدراجات الرباعية وركوب الجمال.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h2 class="m-8 text-center text-4xl font-semibold text-gray-800 ">
+        تضم جنوب السعودية أيضًا العديد من المهرجانات والفعاليات الثقافية التي
+        تسلط الضوء على التراث السعودي والفنون المحلية. إذا كنت ترغب في تجربة
+        مغامرة فريدة، فإن جنوب السعودية هي الوجهة المثالية
+      </h2>
     </section>
   );
 }
